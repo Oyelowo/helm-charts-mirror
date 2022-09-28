433ca2061394476dc266468791d27406a592542d
oyelowo <oyelowooyedayo@gmail.com>
2022-09-28 21:30:48 +0300

    Publish seaweed and fluvio charts

---
+++ publish.sh
@@ -0,0 +1,4 @@
+ helm template -f ./fluvio/k8-util/helm/fluvio-app/values.yaml fluvio-app  ./fluvio/k8-util/helm/fluvio-app
+
+
+helm template -f ./fluvio/k8-util/helm/fluvio-sys/values.yaml fluvio-sys  ./fluvio/k8-util/helm/fluvio-sys
