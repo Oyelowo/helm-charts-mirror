842262bd3701852ea7c2be08f71ad0fb3dba1eb8
oyelowo <oyelowooyedayo@gmail.com>
2022-09-30 07:47:33 +0300

    Add seaweed publish steps

--- publish.sh
+++ publish.sh
@@ -2,3 +2,10 @@
 
 
 helm template -f ./fluvio/k8-util/helm/fluvio-sys/values.yaml fluvio-sys  ./fluvio/k8-util/helm/fluvio-sys
+
+
+
+
+helm lint helm_charts2
+helm package helm_charts2
+helm repo index --url https://seaweedfs.github.io/<repo-name>/ .
